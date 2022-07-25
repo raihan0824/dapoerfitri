3388962e48542b7c3cba844a63d4b79dd58dc6b9
raihan0824 <84271135+raihan0824@users.noreply.github.com>
2022-07-25 10:29:06 +0700

    update form position for mobile

--- src/components/Navbar/Form/index.jsx
+++ src/components/Navbar/Form/index.jsx
@@ -64,6 +64,34 @@ const Modal = ({ onClose, children, title }) => {
                                 />
                             </div>
                         </form>
+
+                        <form className={styles.form_mobile}>
+                            <img className={styles.close} src="https://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/16/yellow-cross-icon.png" onClick={() => div_hide()} />
+                            <h2>Order now!</h2>
+                            <hr />
+
+                            <input id="wa_name" className={styles.wa_name} name="name" placeholder="Name" type="text" />
+                            <textarea id="wa_textarea" className={styles.wa_textarea} name="address" placeholder="Alamat"></textarea>
+
+                            <div className={styles.datainput}>
+                                <select id="wa_select" className={styles.wa_select}>
+                                    <option hidden='hidden' defaultValue='default'>Pilih Menu</option>
+                                    <option defaultValue="1">Menu 1</option>
+                                    <option defaultValue="2">Menu 2</option>
+                                    <option defaultValue="3">Menu 3</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <input
+                                    className={styles.submit}
+                                    type="submit"
+                                    name="submit"
+                                    value="Send"
+                                    onClick={() => gotowhatsapp()}
+                                />
+                            </div>
+                        </form>
                         </StyledModalWrapper>
                     </div>            
                 </div>        
