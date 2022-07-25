f2be1f0b17f620f00db49d28dcdb8d1d2d0144b3
raihan0824 <84271135+raihan0824@users.noreply.github.com>
2022-07-25 10:45:43 +0700

    update form mobile position

--- src/components/Navbar/Form/index.jsx
+++ src/components/Navbar/Form/index.jsx
@@ -36,64 +36,69 @@ const Modal = ({ onClose, children, title }) => {
             <body id='body'>
                 <div className={styles.abc} id='abc'>
                     <div className={styles.popupContact}>
-                    <StyledModalWrapper ref={modalWrapperRef}>
-                        <form className={styles.form}>
-                            <img className={styles.close} src="https://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/16/yellow-cross-icon.png" onClick={() => div_hide()} />
-                            <h2>Order now!</h2>
-                            <hr />
+                        <StyledModalWrapper ref={modalWrapperRef}>
+                            <form className={styles.form}>
+                                <img className={styles.close} src="https://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/16/yellow-cross-icon.png" onClick={() => div_hide()} />
+                                <h2>Order now!</h2>
+                                <hr />
 
-                            <input id="wa_name" className={styles.wa_name} name="name" placeholder="Name" type="text" />
-                            <textarea id="wa_textarea" className={styles.wa_textarea} name="address" placeholder="Alamat"></textarea>
+                                <input id="wa_name" className={styles.wa_name} name="name" placeholder="Name" type="text" />
+                                <textarea id="wa_textarea" className={styles.wa_textarea} name="address" placeholder="Alamat"></textarea>
 
-                            <div className={styles.datainput}>
-                                <select id="wa_select" className={styles.wa_select}>
-                                    <option hidden='hidden' defaultValue='default'>Pilih Menu</option>
-                                    <option defaultValue="1">Menu 1</option>
-                                    <option defaultValue="2">Menu 2</option>
-                                    <option defaultValue="3">Menu 3</option>
-                                </select>
-                            </div>
+                                <div className={styles.datainput}>
+                                    <select id="wa_select" className={styles.wa_select}>
+                                        <option hidden='hidden' defaultValue='default'>Pilih Menu</option>
+                                        <option defaultValue="1">Menu 1</option>
+                                        <option defaultValue="2">Menu 2</option>
+                                        <option defaultValue="3">Menu 3</option>
+                                    </select>
+                                </div>
 
-                            <div>
-                                <input
-                                    className={styles.submit}
-                                    type="submit"
-                                    name="submit"
-                                    value="Send"
-                                    onClick={() => gotowhatsapp()}
-                                />
-                            </div>
-                        </form>
-                        {/* For Mobile */}
-                        <form className={styles.form_mobile}>
-                            <img className={styles.close} src="https://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/16/yellow-cross-icon.png" onClick={() => div_hide()} />
-                            <h2>Order now!</h2>
-                            <hr />
-
-                            <input id="wa_name" className={styles.wa_name} name="name" placeholder="Name" type="text" />
-                            <textarea id="wa_textarea" className={styles.wa_textarea} name="address" placeholder="Alamat"></textarea>
-
-                            <div className={styles.datainput}>
-                                <select id="wa_select" className={styles.wa_select}>
-                                    <option hidden='hidden' defaultValue='default'>Pilih Menu</option>
-                                    <option defaultValue="1">Menu 1</option>
-                                    <option defaultValue="2">Menu 2</option>
-                                    <option defaultValue="3">Menu 3</option>
-                                </select>
-                            </div>
-
-                            <div>
-                                <input
-                                    className={styles.submit}
-                                    type="submit"
-                                    name="submit"
-                                    value="Send"
-                                    onClick={() => gotowhatsapp()}
-                                />
-                            </div>
-                        </form>
+                                <div>
+                                    <input
+                                        className={styles.submit}
+                                        type="submit"
+                                        name="submit"
+                                        value="Send"
+                                        onClick={() => gotowhatsapp()}
+                                    />
+                                </div>
+                            </form>
                         </StyledModalWrapper>
-                    </div>            
+                    </div> 
+
+                    <div className={styles.popupContactMobile}>
+                        <StyledModalWrapper ref={modalWrapperRef}>
+                            <form className={styles.form_mobile}>
+                                <img className={styles.close} src="https://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/16/yellow-cross-icon.png" onClick={() => div_hide()} />
+                                <h2>Order now!</h2>
+                                <hr />
+
+                                <input id="wa_name" className={styles.wa_name} name="name" placeholder="Name" type="text" />
+                                <textarea id="wa_textarea" className={styles.wa_textarea} name="address" placeholder="Alamat"></textarea>
+
+                                <div className={styles.datainput}>
+                                    <select id="wa_select" className={styles.wa_select}>
+                                        <option hidden='hidden' defaultValue='default'>Pilih Menu</option>
+                                        <option defaultValue="1">Menu 1</option>
+                                        <option defaultValue="2">Menu 2</option>
+                                        <option defaultValue="3">Menu 3</option>
+                                    </select>
+                                </div>
+
+                                <div>
+                                    <input
+                                        className={styles.submit}
+                                        type="submit"
+                                        name="submit"
+                                        value="Send"
+                                        onClick={() => gotowhatsapp()}
+                                    />
+                                </div>
+                            </form>
+                        </StyledModalWrapper>
+                    </div>    
+
                 </div>        
             </body>
             <Head>
